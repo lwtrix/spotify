9361612c5d67d8da8a5591a649b97b0d2a86b870
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-11-15 15:02:34 +0000

    dynamically adding track list

--- artist.js
+++ artist.js
@@ -4,21 +4,38 @@ const artistNamePlaylist = document.getElementById("artist-name-play")
 const artistContainer = document.getElementById("artist-main")
 const artistCircle = document.getElementById("circle")
 const smallAlbum = document.getElementById("album-small")
-
+const trackList = document.getElementById("track-list")
 const artistId = window.location.search.split('?')[1]
-const searh = new URLSearchParams(artistId)
-const id = searh.get('id')
+const search = new URLSearchParams(artistId)
+let id = search.get('id')
+
 
 const loadArtist = () => {
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${id}`,
     {
-        method: "GET"
+        method: "GET",
     })
     .then((response) => response.json())
     .then((artist) => {
         display(artist)
     })
 
+    .catch((err) => {
+        console.error(err)
+    })
+}
+
+const loadTracks = (artistName) => {
+    fetch(
+        `https://striveschool-api.herokuapp.com/api/deezer/search?q=${artistName}`,
+        {
+          method: "GET",
+        }
+    )
+    .then ((response) => response.json())
+    .then((response) => {
+        displayTracks(response.data)
+    })
     .catch((err) => {
         console.error(err)
     })
@@ -32,11 +49,29 @@ const display = (artist) => {
     artistContainer.style.backgroundImage = `url(${artist.picture_xl})`
     artistCircle.style.backgroundImage = `url(${artist.picture_small})`
     smallAlbum.src = artist.picture_medium
+    loadTracks(artist.name)
 }
 
+const displayTracks = (tracks) => {
+    tracks.forEach((track, index) => {
+        let tr = document.createElement("tr")
+        tr.innerHTML = `<td>${index + 1}</td>
+        <td style = "width: 200px"><img src="${track.album.cover_small}"></td>
+        <td style = "width: 400px">${track.title}</td>
+        <td >${defineDuration(track.duration)}</td>`
+        trackList.appendChild(tr)
+    })
+}
 
+const defineDuration = (t) => {
+    return Math.floor(t / 60) + ':' + ('0' + Math.floor(t % 60)).slice(-2)
+}
 
+function play() {
+    const playBtn = document.getElementById("green-circle-inner")
+    playBtn.innerHTML = `<i class="fa-solid fa-play"></i>`
+}
 
 window.onload = () => {
-    loadArtist()
+    loadArtist(id)
 }
